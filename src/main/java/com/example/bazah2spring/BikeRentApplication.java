6865198b3b76ee1088e011f070bcb59cf1b9ed14
przemek <pwenderholm@gmail.com>
2023-02-12 16:34:43 +0100

    Initial commit

---
+++ src/main/java/com/example/bazah2spring/BikeRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.bazah2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaH2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazaH2SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bazah2spring/BikeRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bazah2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaH2SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
